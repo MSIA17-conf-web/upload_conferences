e396f26a5b09d0ec94c2e4f47f7c7502d245ecaf
Willem Houm <willem.houm@gmail.com>
2019-10-13 17:52:54 +0000

    Bower + px-css + material design

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UploadFormComponent } from './components/upload-form/upload-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'upload',   component: UploadFormComponent },
+  { path: '',   redirectTo: '/upload', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadFormComponent } from './components/upload-form/upload-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadFormComponent
   ],
   imports: [
     BrowserModule,
